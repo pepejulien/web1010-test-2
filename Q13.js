0c237148c1abc2e1072f1b16a18d097ab6a64938
Jose Julien <pepejulienbarriere@gmail.com>
2017-07-19 02:43:34 +0200

    test 2 completed

---
+++ Q13.js
@@ -0,0 +1,9 @@
+// Write a for loop that prints to the console all the numbers from 1 to 10 in
+// ascending order.
+
+var num = 1
+
+while (num <= 10) {
+    console.log(num);
+    num++
+}
---
+++ Q17.js
@@ -0,0 +1,9 @@
+// Create a for loop that prints all the even numbers from 1 to 20 to the console.
+// Use an if statement and the modulo operator in your solution.
+
+for (var num = 20; num >= 1; num--) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+
+}
